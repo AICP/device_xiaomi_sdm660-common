StagOS-Devices/android_device_xiaomi_sdm660-common

sdm660: XiaomiParts: Add spectrum QS Tile

XiaomiParts/src/org/lineageos/settings/device/PerformanceTileService.java
@@ -1,0 +1,38 @@
+package org.lineageos.settings.device;
+
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+public class PerformanceTileService extends TileService {
+
+    @Override
+    public void onStartListening() {
+        int currentState = FileUtils.getProp(DeviceSettings.SPECTRUM_SYSTEM_PROPERTY, 2);
+
+        Tile tile = getQsTile();
+        tile.setState(Tile.STATE_ACTIVE);
+        tile.setLabel(getResources().getStringArray(R.array.spectrum_profiles)[currentState]);
+
+        tile.updateTile();
+        super.onStartListening();
+    }
+
+    @Override
+    public void onClick() {
+        int currentState = FileUtils.getProp(DeviceSettings.SPECTRUM_SYSTEM_PROPERTY, 2);
+
+        int nextState;
+        if (currentState == 4) {
+            nextState = 0;
+        } else {
+            nextState = currentState + 1;
+        }
+
+        Tile tile = getQsTile();
+        FileUtils.setProp(DeviceSettings.SPECTRUM_SYSTEM_PROPERTY, nextState);
+        tile.setLabel(getResources().getStringArray(R.array.spectrum_profiles)[nextState]);
+
+        tile.updateTile();
+        super.onClick();
+    }
+}

AICP/device_xiaomi_sdm660-common

XiaomiParts/src/org/lineageos/settings/device/PerformanceTileService.java
@@ -1,0 +1,38 @@
+package org.lineageos.settings.device;
+
+import android.service.quicksettings.Tile;
+import android.service.quicksettings.TileService;
+
+public class PerformanceTileService extends TileService {
+
+    @Override
+    public void onStartListening() {
+        int currentState = FileUtils.getintProp(DeviceSettings.SPECTRUM_SYSTEM_PROPERTY, 0);
+
+        Tile tile = getQsTile();
+        tile.setState(Tile.STATE_ACTIVE);
+        tile.setLabel(getResources().getStringArray(R.array.spectrum_profiles)[currentState]);
+
+        tile.updateTile();
+        super.onStartListening();
+    }
+
+    @Override
+    public void onClick() {
+        int currentState = FileUtils.getintProp(DeviceSettings.SPECTRUM_SYSTEM_PROPERTY, 0);
+
+        int nextState;
+        if (currentState == 4) {
+            nextState = 0;
+        } else {
+            nextState = currentState + 1;
+        }
+
+        Tile tile = getQsTile();
+        FileUtils.setintProp(DeviceSettings.SPECTRUM_SYSTEM_PROPERTY, nextState);
+        tile.setLabel(getResources().getStringArray(R.array.spectrum_profiles)[nextState]);
+
+        tile.updateTile();
+        super.onClick();
+    }
+}