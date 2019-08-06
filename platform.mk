d62002aa0fbae0c6aabe98e7b4e217c21498addc
nebrassy <nebras30@gmail.com>
2019-08-06 22:59:02 +0300

    update fingerprint to PQ3A.190801.002
    Change-Id: I2b97cce2cf763353093351cd7982930520f3b1f9

--- platform.mk
+++ platform.mk
@@ -42,9 +42,9 @@ DEVICE_PACKAGE_OVERLAYS := device/xiaomi/sdm660-common/overlay
 
 # Build Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
-	PRIVATE_BUILD_DESC="taimen-user 9 PQ3A.190705.001 5565753 release-keys"
+	PRIVATE_BUILD_DESC="taimen-user 9 PQ3A.190801.002 5670241 release-keys"
 
-BUILD_FINGERPRINT := google/taimen/taimen:9/PQ3A.190705.001/5565753:user/release-keys
+BUILD_FINGERPRINT := google/taimen/taimen:9/PQ3A.190801.002/5670241:user/release-keys
 
 # Alipay
 PRODUCT_PACKAGES += \
