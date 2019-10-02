4e030054f79394eabeac6e1733171d43d27348e1
nebrassy <nebras30@gmail.com>
2019-10-02 16:35:19 +0200

    sdm660-common: migrate to power HAL 1.2
    Change-Id: I5ddcfced7b6be8666a97f7408da4af1a980ffc93

--- sdm660.mk
+++ sdm660.mk
@@ -366,7 +366,7 @@ PRODUCT_PACKAGES += \
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power@1.1-service-qti
+    android.hardware.power@1.2-service-qti
 
 # Preopt SystemUI
 PRODUCT_DEXPREOPT_SPEED_APPS += SystemUI
